7d76ba582ad99267bb3c522162c1567403978004
olesya-kuzmina <lesechka.kuzmina@gmail.com>
2024-04-19 16:15:58 +0400

    Corrected Task05.java

--- src/Task05.java
+++ src/Task05.java
@@ -1,7 +1,7 @@
 public class Task05 {
     public static void main(String[] args) {
         System.out.println("Задание №5:");
-        int RADIUS = 5;
+        final int RADIUS = 5;
         double area = Math.PI * (RADIUS * RADIUS);
         System.out.println("Площадь круга = " + Math.round(area));
 
