742f8b6b5c3674810f39d92e5ad311d86703ac4b
olesya-kuzmina <lesechka.kuzmina@gmail.com>
2024-04-19 15:25:13 +0400

    Add Task04.java

--- src/Task04.java
+++ src/Task04.java
@@ -1,6 +1,6 @@
 public class Task04 {
     public static void main(String[] args) {
         System.out.println("Задание №4:");
-        System.out.println("Hello\u0020World");
+        System.out.println("Hello\u0020World!");
     }
 }
